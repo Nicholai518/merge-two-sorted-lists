adf05b9b7b7e35f486756f30e20f8e6307870c79
Nicholas <n.paz4444@gmail.com>
2021-12-24 14:25:47 -0500

    created ListNode class and Lists from example one

---
+++ src/main/java/ListNode.java
@@ -0,0 +1,12 @@
+public class ListNode {
+	// fields
+	int val;
+	ListNode next;
+
+	// constructors
+	ListNode() {}
+	ListNode(int val) { this.val = val; }
+	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+	// methods
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,25 @@
 public class Main {
-  public static class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-	  ListNode(int val) { this.val = val; }
-	  ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
 
 	public static void main(String[] args){
 
+		// Example 1
+		// List 1: 1, 2, 4
+		ListNode listOneNodeThree = new ListNode(4);
+		ListNode listOneNodeTwo = new ListNode(2, listOneNodeThree);
+		ListNode listOneHead = new ListNode(1, listOneNodeTwo);
+
+
+		// List 2: 1, 3, 4
+		ListNode listTwoNodeThree = new ListNode(4);
+		ListNode listTwoNodeTwo = new ListNode(3, listTwoNodeThree);
+		ListNode listTwoHead = new ListNode(1, listTwoNodeTwo);
+
+		ListNode mergedListHead = mergeTwoLists(listOneHead, listTwoHead );
+
+
 	}
 
-	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+	public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 		ListNode listNode = new ListNode();
 		return listNode;
 	}
