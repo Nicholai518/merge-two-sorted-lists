d85b786fc2b1809f8a3ea2f574bea86d4f23df83
Nicholas <n.paz4444@gmail.com>
2021-12-24 14:11:01 -0500

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+	  ListNode(int val) { this.val = val; }
+	  ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+	public static void main(String[] args){
+
+	}
+
+	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		ListNode listNode = new ListNode();
+		return listNode;
+	}
+}
